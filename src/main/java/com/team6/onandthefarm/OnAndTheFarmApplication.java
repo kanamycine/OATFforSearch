bf73917f2f4274a8ea55c6f253d498c79584a88f
kanamycine <jinyeong5320@gmail.com>
2022-09-15 17:45:19 +0900

    feat : INIT COMMIT

---
+++ src/main/java/com/team6/onandthefarm/OnAndTheFarmApplication.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnAndTheFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnAndTheFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team6/onandthefarm/OnAndTheFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnAndTheFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
