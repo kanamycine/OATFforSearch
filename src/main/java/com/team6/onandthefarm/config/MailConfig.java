05103e326e85e3103dbc1a382b51aef88a3c84e1
ksh940925 <ksh9409255@naver.com>
2022-09-19 12:18:31 +0900

    2차인증번호 변경

--- src/main/java/com/team6/onandthefarm/config/MailConfig.java
+++ src/main/java/com/team6/onandthefarm/config/MailConfig.java
@@ -1,7 +1,9 @@
 package com.team6.onandthefarm.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -9,6 +11,13 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfig {
+
+    Environment env;
+    @Autowired
+    public MailConfig(Environment env) {
+        this.env = env;
+    }
+
     @Bean(name="mailSender")
     public JavaMailSenderImpl getJavaMailSender() {
         Properties properties = new Properties();
@@ -24,7 +33,7 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername("ksh9409255@gmail.com");
-        mailSender.setPassword("ulhqjxiaixqbbqdy"); // 2차 인증 비밀번호임
+        mailSender.setPassword(env.getProperty("gmail.passwd")); // 2차 인증 비밀번호임
         mailSender.setDefaultEncoding("utf-8");
         mailSender.setJavaMailProperties(properties);
 
