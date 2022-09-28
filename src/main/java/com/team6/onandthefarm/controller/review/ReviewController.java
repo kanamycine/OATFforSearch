7ddcfb292733ae47d0d7d90879ef8c2c616dded6
kanamycine <jinyeong5320@gmail.com>
2022-09-28 15:17:33 +0900

    feat : review 좋아요, 좋아요 취소 기능 구현

--- src/main/java/com/team6/onandthefarm/controller/review/ReviewController.java
+++ src/main/java/com/team6/onandthefarm/controller/review/ReviewController.java
@@ -2,11 +2,11 @@ package com.team6.onandthefarm.controller.review;
 
 import java.util.List;
 
-import org.apache.coyote.Response;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.team6.onandthefarm.dto.review.ReviewDeleteDto;
 import com.team6.onandthefarm.dto.review.ReviewFormDto;
+import com.team6.onandthefarm.dto.review.ReviewLikeCancelFormDto;
+import com.team6.onandthefarm.dto.review.ReviewLikeFormDto;
 import com.team6.onandthefarm.dto.review.ReviewUpdateFormDto;
 import com.team6.onandthefarm.entity.review.Review;
+import com.team6.onandthefarm.entity.review.ReviewLike;
 import com.team6.onandthefarm.service.review.ReviewService;
 import com.team6.onandthefarm.util.BaseResponse;
 import com.team6.onandthefarm.vo.review.ReviewDeleteRequest;
 import com.team6.onandthefarm.vo.review.ReviewFormRequest;
+import com.team6.onandthefarm.vo.review.ReviewLikeCancelFormRequest;
+import com.team6.onandthefarm.vo.review.ReviewLikeFormRequest;
 import com.team6.onandthefarm.vo.review.ReviewSelectionResponse;
 import com.team6.onandthefarm.vo.review.ReviewUpdateFormRequest;
 
@@ -91,7 +96,43 @@ public class ReviewController {
 
 		return new ResponseEntity(baseResponse, HttpStatus.OK);
 	}
+	// review like
+	@PostMapping(value="like/up")
+	@ApiOperation("리뷰 좋아요 +1")
+	public ResponseEntity<BaseResponse<ReviewLike>> upReviewLikeCount(@RequestBody ReviewLikeFormRequest reviewLikeFormRequest) throws Exception{
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
+		ReviewLikeFormDto reviewLikeFormDto = modelMapper.map(reviewLikeFormRequest, ReviewLikeFormDto.class);
+		Long reviewLikeId = reviewService.upLikeCountReview(reviewLikeFormDto);
+
+		BaseResponse baseResponse = BaseResponse.builder()
+				.httpStatus(HttpStatus.CREATED)
+				.message("up reviewLike Counts completed")
+				.data(reviewLikeId)
+				.build();
+
+		return new ResponseEntity(baseResponse, HttpStatus.OK);
+	}
+
+	@DeleteMapping(value="like/cancel")
+	@ApiOperation("리뷰 좋아요 취소 -1")
+	public ResponseEntity<BaseResponse<ReviewLike>> cancelReviewLikeCount(@RequestBody ReviewLikeCancelFormRequest reviewLikeCancelFormRequest) throws Exception{
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+		ReviewLikeCancelFormDto reviewLikeCancelFormDto = modelMapper.map(reviewLikeCancelFormRequest, ReviewLikeCancelFormDto.class);
+		Long reviewLikeId = reviewService.cancelReviewLikeCount(reviewLikeCancelFormDto);
+
+		BaseResponse baseResponse = BaseResponse.builder()
+				.httpStatus(HttpStatus.OK)
+				.message("Cancel reviewLike counts completed")
+				.data(reviewLikeId)
+				.build();
+		return new ResponseEntity(baseResponse, HttpStatus.OK);
+	}
+
+	//review like cancle
 	@GetMapping("list/orderby/likecount/{productId}/{page-no}")
 	public ResponseEntity<BaseResponse<List<ReviewSelectionResponse>>> getReviewListByLikeCount(@PathVariable("productId") Long productId, @PathVariable("page-no") String pageNumber){
 
---
+++ src/main/java/com/team6/onandthefarm/dto/review/ReviewLikeCancelFormDto.java
@@ -0,0 +1,11 @@
+package com.team6.onandthefarm.dto.review;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ReviewLikeCancelFormDto {
+	private Long reviewId;
+	private Long userId;
+}
