11eccdd72711e4aa7963068d6528e2b35193f42c
jikimomo <jikimomo@gmail.com>
2022-10-13 18:12:21 +0900

    feat : product 상세페이지 조회 때 상품 이미지 추가

--- src/main/java/com/team6/onandthefarm/repository/product/ProductImgRepository.java
+++ src/main/java/com/team6/onandthefarm/repository/product/ProductImgRepository.java
@@ -1,7 +1,12 @@
 package com.team6.onandthefarm.repository.product;
 
+import com.team6.onandthefarm.entity.product.Product;
 import com.team6.onandthefarm.entity.product.ProductImg;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductImgRepository extends CrudRepository<ProductImg, Long> {
+
+    List<ProductImg> findByProduct(Product product);
 }
