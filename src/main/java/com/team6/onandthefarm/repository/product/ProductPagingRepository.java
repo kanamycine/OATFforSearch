3938fc5a9203831f9cdd879e068b57407849c99b
kanamycine <jinyeong5320@gmail.com>
2022-10-14 09:33:32 +0900

    fix : pagination 로직 수정 - 현재, 총 페이지 response에 추가 - 총 요소들 수 response에 추가

--- src/main/java/com/team6/onandthefarm/repository/product/ProductPagingRepository.java
+++ src/main/java/com/team6/onandthefarm/repository/product/ProductPagingRepository.java
@@ -1,42 +1,50 @@
 
 package com.team6.onandthefarm.repository.product;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.team6.onandthefarm.entity.product.Product;
-import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 
-import java.util.List;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductPagingRepository extends PagingAndSortingRepository<Product, Long> {
 
-    @Query("select p from Product p join fetch p.category join fetch p.seller")
-    List<Product> findAllProductOrderByNewest(PageRequest pageRequest);
+	@Query(value = "select p from Product p join fetch p.category join fetch p.seller",
+			countQuery = "select count(p) from Product")
+	Page<Product> findAllProductOrderByNewest(PageRequest pageRequest);
 
-    @Query("select p from Product p join fetch p.category join fetch p.seller")
-    List<Product> findProductListByHighPrice(PageRequest pageRequest);
+	@Query(value = "select p from Product p join fetch p.category join fetch p.seller",
+			countQuery = "select count(p) from Product")
+	Page<Product> findProductListByHighPrice(PageRequest pageRequest);
 
-    @Query("select p from Product p join fetch p.category join fetch p.seller")
-    List<Product> findProductListByLowPrice(PageRequest pageRequest);
+	@Query(value = "select p from Product p join fetch p.category join fetch p.seller",
+			countQuery = "select count(p) from Product")
+	Page<Product> findProductListByLowPrice(PageRequest pageRequest);
 
-    @Query("select p from Product p join fetch p.category join fetch p.seller")
-    List<Product> findProductBySoldCount(PageRequest pageRequest);
+	@Query(value = "select p from Product p join fetch p.category join fetch p.seller",
+			countQuery = "select count(p) from Product")
+	Page<Product> findProductBySoldCount(PageRequest pageRequest);
 
-    @Query("select p from Product p join fetch p.category join fetch p.seller where p.seller.sellerId =:sellerId")
-    List<Product> findProductBySellerNewest(PageRequest pageRequest, @Param("sellerId") Long sellerId);
+	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.seller.sellerId =:sellerId",
+			countQuery = "select count(p) from Product")
+	Page<Product> findProductBySellerNewest(PageRequest pageRequest, @Param("sellerId") Long sellerId);
 
-    @Query("select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category")
-    List<Product> findProductsByCategoryNewest(PageRequest pageRequest,@Param("category") String category);
+	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category",
+			countQuery = "select count(p) from Product")
+	Page<Product> findProductsByCategoryNewest(PageRequest pageRequest, @Param("category") String category);
 
-    @Query("select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category")
-    List<Product> findProductByCategoryHighest(PageRequest pageRequest, @Param("category") String category);
+	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category",
+			countQuery = "select count(p) from Product")
+	Page<Product> findProductByCategoryHighest(PageRequest pageRequest, @Param("category") String category);
 
-    @Query("select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category")
-    List<Product> findProductByCategoryLowest(PageRequest pageRequest, @Param("category") String category);
+	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category",
+			countQuery = "select count(p) from Product")
+	Page<Product> findProductByCategoryLowest(PageRequest pageRequest, @Param("category") String category);
 
-    @Query("select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category")
-    List<Product> findProductByCategorySoldCount(PageRequest pageRequest, @Param("category") String category);
+	@Query(value = "select p from Product p join fetch p.category join fetch p.seller where p.category.categoryName =:category",
+			countQuery = "select count(p) from Product")
+	Page<Product> findProductByCategorySoldCount(PageRequest pageRequest, @Param("category") String category);
 }
