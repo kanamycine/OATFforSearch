67b8d334d1788733a93b1c0cdf584113d1882366
ksh940925 <ksh9409255@naver.com>
2022-10-17 19:32:15 +0900

    fix: feed 내용 길이 수정

--- src/main/java/com/team6/onandthefarm/entity/sns/Feed.java
+++ src/main/java/com/team6/onandthefarm/entity/sns/Feed.java
@@ -3,10 +3,7 @@ package com.team6.onandthefarm.entity.sns;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Builder
 @Slf4j
@@ -27,6 +24,7 @@ public class Feed {
 
     private String feedTitle;
 
+    @Column(length=1000)
     private String feedContent;
 
     private Integer feedViewCount;
