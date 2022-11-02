8dcd738fd6fd10170f28f846b6eb3cf325c54730
kanamycine <jinyeong5320@gmail.com>
2022-11-02 21:33:42 +0900

    feat : 전시구좌카테고리별 전시구좌 조회 구현

---
+++ src/main/java/com/team6/onandthefarm/vo/exhibition/ExhibitionAccountResponse.java
@@ -0,0 +1,18 @@
+package com.team6.onandthefarm.vo.exhibition;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExhibitionAccountResponse {
+	private Long exhibitionAccountId;
+	private String exhibitionAccountName;
+	private boolean exhibitionAccountStatus;
+}
